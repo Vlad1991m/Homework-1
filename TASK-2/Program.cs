6c2b827922484b9844ac0bbb67f941dfe32fd033
Vlad <vlad.melnik@bk.ru>
2022-06-26 15:18:24 +0300

    Task-2 added

---
+++ TASK-2/Program.cs
@@ -0,0 +1,21 @@
+﻿int num1, num2, num3, Max;
+Console.WriteLine("Input the first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input the second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input the third number: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+if(num1>=num2)
+{
+Max=num1;
+}    
+else
+{
+Max=num2;
+} 
+if(num3>Max)
+{
+Max=num3;
+}
+Console.WriteLine("Max is " + Max);
+
