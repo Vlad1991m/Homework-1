eb19721abedf5ecba68493dffa15d030613c32ce
Vlad <vlad.melnik@bk.ru>
2022-06-26 15:22:38 +0300

    Task-3 added

---
+++ TASK-3/Program.cs
@@ -0,0 +1,11 @@
+﻿int num;
+Console.WriteLine("Input the number: ");
+num = Convert.ToInt32(Console.ReadLine());
+if(num%2 ==0)
+{
+    Console.WriteLine("The number is even");
+}
+else
+{ 
+    Console.WriteLine("The number is odd");
+}
