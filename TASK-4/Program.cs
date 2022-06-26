7ed3ea59ff0f0630f378684acb07de22a1707ff0
Vlad <vlad.melnik@bk.ru>
2022-06-26 15:23:34 +0300

    Task-4 added

---
+++ TASK-4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Input a number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+if(N<=1)
+Console.WriteLine("Input number is invalid, use one starting from number 1");
+while (current <= N)
+{
+    if (current % 2 == 0)
+    {
+        Console.Write(current + " ");
+        current++;
+    }
+    current++;
+} 
+
+
